ee9c2ca4c9d9e695ee23ae67f2ec22f9129abcb2
Amalvir <samalonsovirissel@gmail.com>
2020-11-29 17:14:32 +0100

    Plus de debug.txt

--- sous_prog.f90
+++ sous_prog.f90
@@ -74,9 +74,6 @@ subroutine ecriture(a)
 	integer :: i,j
 	
 	open(11, file="sortie.txt")
-	open(12, file="debug.txt")
-	write(12,*) (a%X(i),i=1,a%N)
-	close(12)
 	do j=1,a%Nt
 		write(11,*) (a%C(i,j),i=1,a%N)
 	end do
